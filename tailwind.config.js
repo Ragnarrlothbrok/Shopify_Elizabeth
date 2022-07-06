b78ad067d50c8806fedb604d8736a390d5f928a0
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2022-07-07 03:34:42 +0530

    Updated Readme

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./sections/*.liquid", "./layout/*.liquid", "./snippets/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
